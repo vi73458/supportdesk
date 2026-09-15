f456b2dbee82e9a1e544eed8e0866c05c409795f
Victor Hugo <113782210+vi73458@users.noreply.github.com>
2026-09-15 18:53:24 -0300

    feat: add Spring Boot application entrypoint

---
+++ backend/src/main/java/br/com/victorhugo/supportdesk/SupportdeskApplication.java
@@ -0,0 +1,11 @@
+package br.com.victorhugo.supportdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportdeskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SupportdeskApplication.class, args);
+    }
+}
