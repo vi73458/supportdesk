c96cd5016c130187b66b9ff54ac9603dfb71c5b6
Victor Hugo <113782210+vi73458@users.noreply.github.com>
2026-09-15 18:53:30 -0300

    feat: add ticket status enum

---
+++ backend/src/main/java/br/com/victorhugo/supportdesk/enums/StatusChamado.java
@@ -0,0 +1,10 @@
+package br.com.victorhugo.supportdesk.enums;
+
+public enum StatusChamado {
+    ABERTO,
+    EM_ANALISE,
+    EM_ATENDIMENTO,
+    AGUARDANDO_USUARIO,
+    RESOLVIDO,
+    ENCERRADO
+}
